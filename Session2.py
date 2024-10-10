64aac27fcb3f74015ba77c9b7a7f45cc7e5f27fd
Kaden Lee <140297314+Kaden123Lee@users.noreply.github.com>
2024-10-10 13:49:41 -0700

    Add files via upload
    10/10/2024

---
+++ Session2.py
@@ -0,0 +1,12 @@
+def main():
+    float1 = 12313.12
+    float2 = -32.012123
+    inte1 = 200
+    inte2 = 10
+    
+    print(f"Sum of ({float1}) & ({float2}) = {float1 + float2} Type: {type(float1 + float2)}") 
+    print(f"Diffrence between ({inte1}) & ({inte2}) = {abs(inte1 - inte2)} Type: {type(inte1 + inte2)}")
+    print(f"Product of ({float1}) & ({float2}) = {float1 + inte1} Type: {type(float1 + inte1)}")
+
+if __name__ == "__main__":
+    main()
