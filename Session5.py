1e7cc181cabc956c6a803892dcb9feab1861e4c9
Kaden Lee <140297314+Kaden123Lee@users.noreply.github.com>
2024-10-10 18:00:48 -0700

    Update Session5.py

--- Session5.py
+++ Session5.py
@@ -1,14 +1,15 @@
 import numpy as np
-import math
+step = (np.pi * 2) / 1000
+x = np.arange(0, 2 * np.pi, step)
 def main():
-    step = (np.pi * 2) / 1000
-    x = np.arange(0, 2 * np.pi, step)
     sin_ar = np.sin(x)
-    print(f"Sin(X):{sin_ar} X: {x}")
+    for i in range(1001):
+        print(f"X Value:{x[i]} Sin(x) Value: {np.sin(x[i])}")
 if __name__ == "__main__":
     main()
 #THIS TOOK ME SO LONG HOLY FUCK
 #I WILL NEVER USE NON NUMPY SINS TO DO AYNTHING
+
 
 
 
