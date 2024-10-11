5013b0e1f54b6201671179768528d472a16e2dd6
Kaden Lee <140297314+Kaden123Lee@users.noreply.github.com>
2024-10-10 18:08:19 -0700

    Update Session5.py

--- Session5.py
+++ Session5.py
@@ -3,7 +3,7 @@ step = (np.pi * 2) / 1000
 x = np.arange(0, 2 * np.pi, step)
 def main():
     sin_ar = np.sin(x)
-    for i in range(1001):
+    for i in range(len(x)):
         print(f"X Value:{x[i]} Sin(x) Value: {np.sin(x[i])}")
 if __name__ == "__main__":
     main()
